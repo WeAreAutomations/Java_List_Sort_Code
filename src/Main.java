f0098a8a1aff8725f08d04ada8b45da15cd40350
yanuvelir <yanuvelir@gmail.com>
2023-02-12 14:55:42 +0200

    added extra account weareautomation

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Double> numbers = new ArrayList<>();
+        numbers.add(1.0);
+        numbers.add(77.0);
+        numbers.add(6.0);
+        Collections.sort(numbers);
+
+        System.out.print("Sorted numbers: " + numbers);
+        System.out.print("Sorted numbers: " + numbers);
+        System.out.print("Sorted numbers: " + numbers);
+        System.out.print("Sorted numbers: " + numbers);
+        System.out.println("Sorted numbers LN: " + numbers);
+        System.out.println("Sorted numbers LN: " + numbers);
+        System.out.println("Sorted numbers LN: " + numbers);
+        System.out.println("Sorted numbers LN: " + numbers);
+    }
+}
+
